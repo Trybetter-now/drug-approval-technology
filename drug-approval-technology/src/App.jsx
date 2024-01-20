2e1ef7a4e59fce362eea3e617d541da30c1d86a2
DangThiemLoc <thiemloc02@gmail.com>
2024-01-20 15:34:33 +0700

    'commit'

---
+++ drug-approval-technology/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    // <BrowserRouter>
+    <div className="App">
+      <h1 className="mb-4 text-4xl font-extrabold">
+        Hello world!
+      </h1>
+    </div>
+    // </BrowserRouter>
+  )
+}
+
+export default App

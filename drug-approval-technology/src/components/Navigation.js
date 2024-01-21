1cb33a573261f2721a29eae6789a21d6e7b0c9ed
DangThiemLoc <thiemloc02@gmail.com>
2024-01-21 10:42:41 +0700

    fix config

---
+++ drug-approval-technology/src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div>
+            <h1 style={{ fontSize: 'large', color: 'blue' }}>Navigation</h1>
+        </div>
+    )
+}
+export default Navigation
